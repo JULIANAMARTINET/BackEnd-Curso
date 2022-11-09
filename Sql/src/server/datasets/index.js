1fd10dd485434c9abe1bea5ed384391c8431c2c3
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-11-08 23:26:07 -0300

    Sql desafio clase 16

---
+++ Sql/src/server/datasets/index.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+      title: "sandwichSalado",
+      price: "1",
+      thumbnail:
+        "",
+    },
+    {
+      title: "alfajor de maicena",
+      price: "2",
+      thumbnail:
+        "",
+    },
+    {
+      title: "alfajor ddl",
+      price: "3",
+      thumbnail:
+        "",
+    },
+  ];
+
+  
+    const messages = [
+    {
+       "email": "juliana@hotmail.com",
+       "text": "holaa",
+       "timestamp": "7/11/2022 12:04:09",
+       "id": 1
+    }
+ ]
+
+  export const DATASETS = { products, messages };
--- Sql/src/server/index.js
+++ Sql/src/server/index.js
@@ -1,5 +1,5 @@
 import knex from "knex";
-import { config } from "../config/index.js";
+import { config } from "../../config/index.js";
 import { DATASETS } from "./datasets/index.js";
 
 const KnexMySQL = knex(config.knex.mysql);
